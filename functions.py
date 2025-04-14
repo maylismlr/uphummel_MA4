e31e287f5a2da956c17330ef2e0ce56ba167d0f9
maylismlr <maylis.muller@epfl.ch>
2025-04-14 17:02:51 +0200

    debugging the clustering function (no None in T1)

--- functions.py
+++ functions.py
@@ -158,9 +158,14 @@ def flatten_upper(mat):
         return mat[np.triu_indices_from(mat, k=1)]
 
 def cluster_and_plot(matrices, numerical_cols_names, categorical_cols_name, clusters = 2):
+    '''
+    Remark: T1_matrices cannot be 'None' here !
+    '''
+    
     # Preprocess categorical columns
     matrices[categorical_cols_name] = matrices[categorical_cols_name].fillna('Unknown')  # Handle missing values
     matrices_encoded = pd.get_dummies(matrices[categorical_cols_name], drop_first=True)
+    matrices_encoded = matrices_encoded.dropna(subset=['T1_matrix'])  # Handle NaN values
 
     # Extract numerical column
     numerical_cols = matrices[numerical_cols_names].fillna(0).values.reshape(-1, 1)
